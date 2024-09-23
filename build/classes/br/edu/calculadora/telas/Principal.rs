4274b3cdbe3e9197e3a35b27cc2ef73023b9dc70
Ismael Nascimento <72841488+ismaelnascimento@users.noreply.github.com>
2024-09-23 17:46:05 -0300

    + calculadora IMC pt.2

---
+++ build/classes/br/edu/calculadora/telas/Principal.rs
@@ -0,0 +1,7 @@
+br.edu.principal.Principal$2
+br.edu.principal.Principal$3
+br.edu.principal.Principal$1
+br.edu.principal.Principal$6
+br.edu.principal.Principal$4
+br.edu.principal.Principal$5
+br.edu.principal.Principal
